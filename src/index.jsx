72e9f5562a55e94a8c82e9d504895c02da9c399e
StevenJBurns <stevenjburns@msn.com>
2019-09-04 06:23:52 -0600

    refactor: remove serviceWorker and references to it

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,4 @@ import ReactDOM from 'react-dom';
 import { App } from './App.jsx';
 import './index.scss';
 
-/* import * as serviceWorker from './serviceWorker'; */
-
 ReactDOM.render(<App />, document.querySelector('#react-root'));
-
-/* serviceWorker.unregister(); */
